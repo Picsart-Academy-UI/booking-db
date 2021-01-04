ee7ab1e3f2f659e6ef94397a33b05ec2ef9fc205
adamyanlina <adamyanlina97@gmail.com>
2021-01-04 15:51:36 +0400

    Modified User model

--- src/models/User.js
+++ src/models/User.js
@@ -6,9 +6,11 @@ const { Schema } = mongoose;
 const UserSchema = new Schema({
   first_name: {
     type: String,
+    required: true,
   },
   last_name: {
     type: String,
+    required: true,
   },
   accepted: {
     type: Boolean,
@@ -17,6 +19,7 @@ const UserSchema = new Schema({
   email: {
     lowercase: true,
     type: String,
+    required: true,
     match: [email_reg, 'Please provide a valid email'],
     unique: true,
     index: true,
@@ -33,17 +36,19 @@ const UserSchema = new Schema({
   team_id: {
     type: Schema.Types.ObjectId,
     ref: 'Team',
+    required: true,
   },
   position_id: {
     type: Schema.Types.ObjectId,
     ref: 'Position',
+    required: true,
   },
   is_admin: {
     type: Boolean,
     default: false,
     index: true,
   },
-  push_notifications: [],
+  push_subscriptions: [Object],
 }, { timestamps: true });
 
 module.exports = mongoose.model('user', UserSchema);
