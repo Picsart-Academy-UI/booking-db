f8b969e181f07f6ac0cc0c71b949079cf68fd2b5
adamyanlina <adamyanlina97@gmail.com>
2021-02-01 00:42:30 +0400

    Final fixing

--- src/models/Chair.js
+++ src/models/Chair.js
@@ -5,24 +5,30 @@ const { Schema } = mongoose;
 
 const ChairSchema = new Schema({
   number: {
+    unique: false,
     type: Number,
-    unique: true,
     required: true,
   },
-}, { timestamps: true, versionKey: false });
 
-ChairSchema.pre('deleteOne', { document: false, query: true }, async function(next) {
-  const doc = await this.model.findOne(this.getFilter());
-  await mongoose
-      .model('Table')
-      .updateMany(
-          { chairs_count: { $gte: doc.number } },
-          { $set: { chairs_count: doc.number - 1 } },
-          { new: true, runValidators: true },
-      );
+  table_id: {
+    type: Schema.Types.ObjectId,
+    ref: 'Table'
+  }
+}, { timestamps: true, versionKey: false});
 
-  await mongoose.model('Reservation').deleteMany({ chair_id: doc._id }, next);
-});
+ChairSchema.index({
+  table_id: 1,
+  number: 1
+}, { unique: true });
+
+ChairSchema.pre(
+    'deleteOne',
+    { document: false, query: true },
+    async function(next) {
+      const doc = await this.model.findOne(this.getFilter());
+      await mongoose.model('Reservation').deleteMany({ chair_id: doc._id }, next);
+    }
+);
 
 ChairSchema.plugin(idValidator);
 
